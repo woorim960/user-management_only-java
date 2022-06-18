d44d532a5faf0d81fba1051fe70f53d0ab68f528
woorim960 <woorimprog@gmail.com>
2022-06-18 16:30:13 +0900

    Refactor: 프로그램 시작 부분을 별도로 분리, 로그인/관리자 인증 부분 분리

--- app/src/iducs/java/pim202112061/controller/PimController.java
+++ app/src/iducs/java/pim202112061/controller/PimController.java
@@ -27,33 +27,33 @@ public class PimController {
         this.memberView = new MemberView(); // 클래스 내부 멤버 변수를 참조한다는 의미를 코드만 봐도 알 수 있도록 this 키워드 추가
     }
 
-    public void dispatch() { // 가져오기 : 메뉴보이기, 선택한 메뉴 처리하기, 결과 반환 반복
+    public void dispatch() {
+        this.memberService.readFile(); // 파일로 부터 사용자 목록 정보 읽기, 클래스 내부 멤버 변수를 참조한다는 의미를 코드만 봐도 알 수 있도록 this 키워드 추가
+        this.start(); // 프로그램 시작
+    }
+
+    private void start() { // 가져오기 : 메뉴보이기, 선택한 메뉴 처리하기, 결과 반환 반복
         boolean isLogin = false; // 지역변수는 선언된 블록이 종료되면 메모리에서 사라짐
         boolean isRoot = false;
-        
+
         Scanner sc = new Scanner(System.in); // 키보드 입력을 받아서 분석 반환
-        this.memberService.readFile(); // 파일로 부터 사용자 목록 정보 읽기, 클래스 내부 멤버 변수를 참조한다는 의미를 코드만 봐도 알 수 있도록 this 키워드 추가
 
         int menu = 0;
         do {
             Member sessionMember = (Member) session.get("member");
-            if(sessionMember != null) {
-                isLogin = true; // 로그인 표시
-                if(sessionMember.getEmail().contains("admin"))
-                    isRoot = true; // 관리자 표시
-            } else {
-                isLogin = false;
-                isRoot = false;
-            }
+            isLogin = this.isLogined(sessionMember); // 로그인 여부 검증
+            isRoot = this.isRoot(sessionMember); // 관리자 여부 검증
 
             String msg = "";
             tuiView.showMenu(isLogin, isRoot);
             menu = sc.nextInt(); // 숫자 입력 후 엔터키
             switch(menu) {
-                case 0: msg = "종료";
+                case 0:
+                    msg = "종료";
                     memberService.saveFile(); // memberdb.txt 에 저장
                     break;
-                case 1: msg = "등록";
+                case 1:
+                    msg = "등록";
                     member = new Member();
                     member.setId(sc.nextLong()); // Long
                     member.setEmail(sc.next()); // String
@@ -65,7 +65,8 @@ public class PimController {
                     memberView.printOne(member);
                     memberView.printMsg(msg + "를 성공했습니다.");
                     break;
-                case 2: msg = "로그인";
+                case 2:
+                    msg = "로그인";
                     String id = sc.next();
                     String pw = sc.next();
                     member = (Member) memberService.login(id, pw);
@@ -79,13 +80,15 @@ public class PimController {
                     else
                         memberView.printMsg("로그인 정보 확인 바랍니다. "); // View 전달
                     break;
-                case 3: msg = "정보조회";
+                case 3:
+                    msg = "정보조회";
                     // printOne : 하나의 member 정보 출력
                     memberView.printOne(memberService.getMember(
                             (Member) session.get("member")));
                     memberView.printMsg(msg + "를 성공했습니다.");
                     break;
-                case 4: msg = "정보수정";
+                case 4:
+                    msg = "정보수정";
                     member = new Member();
                     member.setId(sessionMember.getId()); // id 변경 불가(같은 값으로 설정)
                     member.setEmail(sessionMember.getEmail()); // email 변경 불가
@@ -100,21 +103,24 @@ public class PimController {
                     else
                         System.out.println("수정에 실패하였습니다. ");
                     break;
-                case 5: msg = "로그아웃";
+                case 5:
+                    msg = "로그아웃";
                     memberService.saveFile();
                     memberService.readFile();
                     if(session.get("member") != null) {
                         session.remove("member");
                     }
                     memberView.printMsg(msg + "를 성공했습니다.");
-                break;
-                case 6: msg = "회원탈퇴";
+                    break;
+                case 6:
+                    msg = "회원탈퇴";
                     member = new Member();
                     memberService.deleteMember(member);
                     System.out.println("탈퇴가 되었습니다. ");
                     //    System.out.println("탈퇴에 실패하였습니다. ");
                     break;
-                case 7: msg = "회원목록조회";
+                case 7:
+                    msg = "회원목록조회";
                     memberView.printList(memberService.getMemberList());
                     memberView.printMsg(msg + "를 성공했습니다.");
                     break;
@@ -122,5 +128,13 @@ public class PimController {
                     msg = "입력 코드 확인 :"; break;
             }
         } while(menu != 0);
+    }
+
+    private boolean isLogined(Member sessionMember) {
+        return sessionMember != null ? true : false;
+    }
+
+    private boolean isRoot(Member sessionMember) {
+        return (sessionMember != null) && sessionMember.getEmail().contains("admin") ? true : false;
     }
 }

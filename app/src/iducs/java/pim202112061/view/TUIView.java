f6294685b485e9a17ac5cf31aad45b306d0be78e
woorim960 <woorimprog@gmail.com>
2022-06-18 16:31:30 +0900

    Refactor(TUIView): if 문의 공백이 중구난방인 것을 공통되도록 수정

--- app/src/iducs/java/pim202112061/view/TUIView.java
+++ app/src/iducs/java/pim202112061/view/TUIView.java
@@ -2,11 +2,11 @@ package iducs.java.pim202112061.view;
 
 public class TUIView { //TUI : Text User Interface, CUI(Character UI)
     public void showMenu(boolean isLogin, boolean isRoot) {
-        if(isLogin == false) { // F & F
+        if (isLogin == false) { // F & F
             System.out.print("1. 등록\t");
             System.out.print("2. 로그인\n");
         } else {
-            if(isRoot == false) { // T & F
+            if (isRoot == false) { // T & F
                 System.out.print("3. 정보조회\t");
                 System.out.print("4. 정보수정\t");
                 System.out.print("5. 로그아웃\t");
